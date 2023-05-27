0937d4856178e9c21fa689e28ea048e5e3be5ad0
pdefusco <pauldefusco@gmail.com>
2023-05-26 21:26:22 -0700

    corrected typo in script 1

--- autogen/cml_sparkjob_1.py
+++ autogen/cml_sparkjob_1.py
@@ -121,10 +121,10 @@ spark.sql("SHOW CURRENT NAMESPACE").show()
 #---------------------------------------------------
 
 car_installs_df.writeTo("spark_catalog.{0}_CAR_INSTALLS_{1}".format(dbname, username)).create()
-car_installs_df.writeTo("spark_catalog.{0}_CAR_SALES_{1}".format(dbname, username)).create()
-car_installs_df.writeTo("spark_catalog.{0}_CUSTOMER_DATA_{1}".format(dbname, username)).create()
-car_installs_df.writeTo("spark_catalog.{0}_EXPERIMENTAL_MOTORS_{1}".format(dbname, username)).create()
-car_installs_df.writeTo("spark_catalog.{0}_GEO_DATA_XREF_{1}".format(dbname, username)).create()
+car_sales_df.writeTo("spark_catalog.{0}_CAR_SALES_{1}".format(dbname, username)).create()
+customer_data_df.writeTo("spark_catalog.{0}_CUSTOMER_DATA_{1}".format(dbname, username)).create()
+factory_data_df.writeTo("spark_catalog.{0}_EXPERIMENTAL_MOTORS_{1}".format(dbname, username)).create()
+geo_data_df.writeTo("spark_catalog.{0}_GEO_DATA_XREF_{1}".format(dbname, username)).create()
 
 print("\tPOPULATE TABLE(S) COMPLETED")
 
