5301cb37d67fbf2ac06a148105c61951db4e40b6
pdefusco <pauldefusco@gmail.com>
2023-05-28 16:59:08 -0700

    updated spark session configs for cml

--- autogen/cde_sparkjob_2.py
+++ autogen/cde_sparkjob_2.py
@@ -71,22 +71,42 @@ print("\nUsing DB Name: ", dbname)
 #.config("spark.jars","/app/mount/{}/spark-measure_2.13-0.23.jar".format(CDE_RESOURCE_NAME))\
 #.config("spark.driver.extraClassPath","/app/mount/{}/spark-measure_2.13-0.23.jar".format(CDE_RESOURCE_NAME))\
 
-spark = SparkSession.builder.appName('ENRICH')\
-            .config("spark.yarn.access.hadoopFileSystems", data_lake_name)\
-            .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")\
-            .config("spark.sql.catalog.spark_catalog.type", "hive")\
-            .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")\
-            .getOrCreate()
-
-### CML SPARK SESSION
+CDE_RESOURCE_NAME = "SPARKGEN_FILES"
 
 """spark = SparkSession.builder.appName('ENRICH')\
             .config("spark.yarn.access.hadoopFileSystems", data_lake_name)\
             .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")\
             .config("spark.sql.catalog.spark_catalog.type", "hive")\
             .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")\
-            .config("spark.jars.packages","ch.cern.sparkmeasure:spark-measure_2.12:0.23")\
+            .config("spark.jars","/app/mount/{}/spark-measure_2.13-0.23.jar".format(CDE_RESOURCE_NAME))\
+            .config("spark.driver.extraClassPath","/app/mount/{}/spark-measure_2.13-0.23.jar".format(CDE_RESOURCE_NAME))\
             .getOrCreate()"""
+
+spark = SparkSession.builder.appName('ENRICH')\
+            .config("spark.yarn.access.hadoopFileSystems", data_lake_name)\
+            .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")\
+            .config("spark.sql.catalog.spark_catalog.type", "hive")\
+            .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")\
+            .config("spark.jars.packages","ch.cern.sparkmeasure:spark-measure_2.12:0.23")\
+            .getOrCreate()
+
+
+#.config("spark.jars.packages","ch.cern.sparkmeasure:spark-measure_2.12:0.23")\
+
+print("ALL SPARK CONFIGS IN CDE POST SESSION: ")
+print(spark.sparkContext.getConf().getAll())
+
+
+### CML SPARK SESSION
+
+import cml.data_v1 as cmldata
+
+from pyspark import SparkContext
+SparkContext.setSystemProperty("spark.jars.packages","ch.cern.sparkmeasure:spark-measure_2.12:0.23")
+
+CONNECTION_NAME = "go01-aw-dl"
+conn = cmldata.get_connection(CONNECTION_NAME)
+spark = conn.get_spark_session()
 
 _DEBUG_ = False
 
