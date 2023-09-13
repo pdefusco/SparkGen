e33fa889c487d2fad7b91b8d9927d6fcad831e40
pdefusco <pauldefusco@gmail.com>
2023-09-12 18:08:29 -0700

    backing up progress on pipeline

---
+++ CDE_Runtime/sparkmeasure_test.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    """
+        Usage: pi [partitions]
+    """
+    spark = SparkSession\
+        .builder\
+        .appName("PythonPi")\
+        .getOrCreate()
+        
+    from sparkmeasure import TaskMetrics
+    taskmetrics = TaskMetrics(spark)
+    taskmetrics.runandmeasure(globals(), 'spark.sql("select count(*) from range(1000) cross join range(1000) cross join range(1000)").show()')
+    spark.stop()
